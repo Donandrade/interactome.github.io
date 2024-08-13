f4f2908099f89dc221d5afe43aea4389562e0f68
deandradesilvae <mariodeandradee@gmail.com>
2024-08-13 11:32:33 -0700

    script R to analyse degree and betweenness centrality

---
+++ graph_analyser.R
@@ -0,0 +1,58 @@
+library(igraph)
+
+# Load the interaction data from a text file
+data <- read.table("interactions.txt", header = TRUE)
+
+# Simplify the graph to remove multiple edges and self-loops, and convert it to a data frame
+semRedRede <- as_data_frame(simplify(graph_from_data_frame(data, directed=FALSE)))
+
+# Create an undirected graph from the simplified data frame
+rede <- graph.data.frame(semRedRede, directed = FALSE)
+
+# Calculate the betweenness centrality for each vertex in the network
+gargalo <- betweenness(rede, v = V(rede), directed = FALSE, weights = NULL,
+                       nobigint = TRUE, normalized = FALSE)
+
+# Convert the betweenness centrality to a table and then to a data frame
+gargalo <- as.table(gargalo)
+gargalTable <- as.data.frame(gargalo)
+
+# Save the betweenness centrality data to a text file
+write.table(x=gargalTable, file = "betweenness.txt", sep = "\t",
+            row.names = FALSE, col.names = FALSE, quote = FALSE)
+
+# Display a summary of the betweenness centrality data
+summary(gargalTable)
+
+######################### Degree Centrality ###################
+
+# Calculate the degree centrality for each vertex in the network
+grau <- degree(rede, v = V(rede), loops = TRUE, normalized = FALSE)
+
+# Convert the degree centrality to a table and then to a data frame
+grau <- as.table(grau)
+degreeTable <- as.data.frame(grau)
+
+# Save the degree centrality data to a text file
+write.table(x=degreeTable, file = "degree.txt", sep = "\t",
+            row.names = FALSE, col.names = FALSE, quote = FALSE)
+
+# Display a summary of the degree centrality data
+summary(degreeTable)
+
+######################## Module Detection ###################
+
+# Run the fast greedy algorithm to identify community structure (modules) in the network
+fc = fastgreedy.community(rede)
+# Reference for the method: A Clauset, MEJ Newman, C Moore: Finding community structure in very large networks.
+
+# Save the module memberships for each vertex
+nos = as.data.frame(get.vertex.attribute(rede))
+c = as.data.frame(matrix(0, ncol = 2, nrow = length(nos[,1])))
+c[,1] = nos[,1]
+mod = as.data.frame(fc$membership)
+c[,2] = mod[,1]
+
+# Save the module data to a text file
+write.table(x=c, file = "clusters_select.txt", sep = "\t", row.names = FALSE, col.names = FALSE, quote = FALSE)
+
